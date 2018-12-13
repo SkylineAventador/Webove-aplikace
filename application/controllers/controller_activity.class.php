4d3fb0414c3705a36d3a69cac7a1a14d4bdaa0b5
Kravtsov Dmytro <kravtsov.d@seznam.cz>
2018-12-13 22:11:08 +0100

    Prubezne upravy(prubezny safe commit, userActivity page creation)
    Signed-off-by: Kravtsov Dmytro <kravtsov.d@seznam.cz>

--- application/controllers/controller_activity.class.php
+++ application/controllers/controller_activity.class.php
@@ -14,7 +14,23 @@ class Controller_Activity implements IController{
 
     public function getResult(Twig_Environment $twig, $p_tpl_name, $tplData)
     {
-        //$tplData['activity_data'] = $this->db->getMPArticles();
+        switch ($_SESSION["user_data"]["idprava"]) {
+            //Autor
+            case 0:{
+                $tplData['activity_data'] = $this->db->getActData(0);
+                break;
+            }
+            //Spravce
+            case 2:{
+                $tplData['activity_data'] = $this->db->getActData(2);
+                break;
+            }
+            //Rezenzent
+            case 3:{
+                $tplData['activity_data'] = $this->db->getActData(3);
+                break;
+            }
+        }
 
         $page_template = $twig->loadTemplate($p_tpl_name);
 
--- application/models/model_activity.class.php
+++ application/models/model_activity.class.php
@@ -2,4 +2,36 @@
 
 class Model_Activity extends Model_Database{
 
+    public function getActData($user_idprava)
+    {
+        $returnData = null;
+        switch ($user_idprava) {
+            case 0:
+                $returnData = $this->getAutorData();
+                break;
+            case 2:
+                $returnData = $this->getAdminData();
+                break;
+            case 3:
+                $returnData = $this->getReviewerData();
+                break;
+        }
+
+        return $returnData;
+    }
+
+    private function getAutorData(){
+        $autor_name = $_SESSION['user_data']['jmeno'];
+        $stm = $this->getPdo()->query("SELECT * FROM prispevky 
+                                      WHERE author = \"$autor_name\"");
+        return $stm->fetchAll();
+    }
+
+    private function getAdminData(){
+        return $this->getPdo()->query("");
+    }
+
+    private function getReviewerData(){
+        return $this->getPdo()->query("");
+    }
 }
